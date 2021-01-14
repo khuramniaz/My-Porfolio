bd25e42ee2831640be4141c08be6c9fc83dc75bb
MUHAMMAD KHURAM NIAZ <knbhatti99@gmail.com>
2021-01-14 21:58:18 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,18 +24,21 @@ const Home = () => (
       />
       <Summary data={resume.summary} />
       <div className="border-b border-neutral-300 pb-2 my-5 lg:flex">
-        <div className="lg:w-2/3 lg:pr-8">
-          {resume.experience && <Experience data={resume.experience} />}
-          {resume.projects && <Projects data={resume.projects} />}
-        </div>
+        
         <div className="lg:w-1/3 lg:pl-8 lg:border-l lg:border-neutral-300 ">
-          {resume.skills && <Skills data={resume.skills} />}
           {resume.education && <Education data={resume.education} />}
+          {resume.skills && <Skills data={resume.skills} />}
           {resume.sidebar &&
             resume.sidebar.map(item => (
               <List key={`${item.title}-side`} data={item} />
             ))}
         </div>
+
+        <div className="lg:w-2/3 lg:pr-8">
+          {resume.experience && <Experience data={resume.experience} />}
+          {resume.projects && <Projects data={resume.projects} />}
+        </div>
+      
       </div>
       <Footer social={resume.social} />
     </div>
